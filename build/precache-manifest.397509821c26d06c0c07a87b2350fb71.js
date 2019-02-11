290a5cdde97990127d5ebf58eb1c1d43ce16b4a9
Yitong Xiao <alexiao@foxmail.com>
2019-02-11 16:13:36 -0500

    gitignore

---
+++ build/precache-manifest.397509821c26d06c0c07a87b2350fb71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "35c12016b1d7e91089c484b459b7fd76",
+    "url": "/static/media/white.35c12016.svg"
+  },
+  {
+    "revision": "8cb29d5e3dba4da116a002971ec3f30d",
+    "url": "/static/media/donut.8cb29d5e.svg"
+  },
+  {
+    "revision": "5d0cba1ddcf1852be7cda666b81ae445",
+    "url": "/static/media/cinnamon_sticks.5d0cba1d.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d7c2ed648b771e6aa377",
+    "url": "/static/js/main.d7c2ed64.chunk.js"
+  },
+  {
+    "revision": "fb862df9d835b9fdc3ee",
+    "url": "/static/js/2.fb862df9.chunk.js"
+  },
+  {
+    "revision": "cfae42e2699e693a6be0b045e20f6a1c",
+    "url": "/index.html"
+  }
+];
